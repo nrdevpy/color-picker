aabd27e50e62fed61505a2e149cf7ff080753981
nrdev <nelson_rojas_janda@hotmail.com>
2022-03-17 21:04:26 +0100

    Add change to background to addEventListener

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 const exit = document.getElementById('exit');
 const color = document.forms['form']['color'];
 
-// Event to change hexadecimal color text.
+// Event to change hexadecimal color text and background color.
 color.addEventListener('input', () => {
     const value = color.value;
     
-    exit.innerHTML = value;
-})
+    exit.innerHTML = exit.style.backgroundColor = value;
+});
