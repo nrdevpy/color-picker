807179c4e038456b1be72ff730c770ce21842e9a
nrdev <nelson_rojas_janda@hotmail.com>
2022-03-22 11:47:36 +0100

    Add dynamic load colors and fixed duplicate datas bug

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
-// TODO: Fix bug of adding double colors.
-
 // * Variables.
 const exit = document.getElementById('exit');
 const color = document.forms['form']['color'];
 
 // * Functions.
 // Function to change hexadecimal color text and background color.
+
 const setColor = () => {
+    // console.log ('Seleccionando color en color picker')
     const value = color.value;    
     exit.innerHTML = exit.style.backgroundColor = value;
-    saveColor(value);
+    
+    saveColor (value);
+    setTimeout (loadColors, 100);
 }
 
 // * Events.
--- js/local-storage.js
+++ js/local-storage.js
@@ -4,31 +4,29 @@ data in the browser storage
 */
 
 // * Variables.
-const storage = JSON.parse(localStorage.getItem('colorValue'));
-const arrColor = [];
+let arrColor = [];
 const section = document.getElementById('colorSection');
+
+
+// * Functions.
+const saveColor = (value) => {
+    arrColor.push(value);
+    localStorage.setItem('colorValue', JSON.stringify(arrColor));
+};
 
 // Hook every color of browser storage.
 const loadColors = () => {
+    arrColor = []; // Reset array to not duplicate datas.
+    const storage = JSON.parse(localStorage.getItem('colorValue'));
+
     if (storage !== null) {
-        let insHTML = '';
-        
+        let insHTML = ''; // Variable to insert html.
         for (let i of storage) {
-            console.log (`Taking data... ${i}`);
             arrColor.push(i);
             insHTML += `
                 <div class="colorTest" style="background-color: ${i};">${i}</div>
             `;
         }
-
         section.innerHTML = insHTML;
     }
-}
-
-// * Functions.
-const saveColor = (value) => {
-    console.log (`Adding value: ${value} to the storage`);
-    arrColor.push(value);
-    localStorage.setItem('colorValue', JSON.stringify(arrColor));
-    console.log (`Value added`);
-}
+};
