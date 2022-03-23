8af6a542380dea32bfe1b93f1eaa6b15f3f6977d
nrdev <nelson_rojas_janda@hotmail.com>
2022-03-23 19:59:08 +0100

    Add comment

--- js/app.js
+++ js/app.js
@@ -7,10 +7,8 @@ const exit = document.getElementById('exit');
 const color = document.forms['form']['color'];
 
 // * Functions.
-// Function to change hexadecimal color text and background color.
-
+// Change hexadecimal color text, background color and save color.
 const setColor = () => {
-    // console.log ('Seleccionando color en color picker')
     const value = color.value;    
     exit.innerHTML = exit.style.backgroundColor = value;
     
